5cd2ed11da853048a846f63e34be69127a13b2ab
olehhapuk <oleggapuk20@gmail.com>
2021-02-17 12:23:02 +0200

    Added bootstrap-grid.min.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import './variables.css';
+import './assets/css/bootstrap-grid.min.css';
 
 import { App } from './components/App';
 
